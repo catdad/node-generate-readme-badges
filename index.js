bb502011f7a1f61c7496c4e9632b848478394af8
Kiril Vatev <vatev.1@gmail.com>
2016-06-06 09:33:03 -0400

    adding super :shit: implementation of outputting badges text

---
+++ index.js
@@ -0,0 +1,45 @@
+var _ = require('lodash');
+var argv = require('yargs').argv;
+
+var badgesString = `
+[![Build][1]][2]
+[![Code Climate][5]][6]
+[![Downloads][7]][8]
+[![Version][9]][8]
+[![Dependency Status][10]][11]
+
+[1]: https://travis-ci.org/catdad/{{name}}.svg?branch=master
+[2]: https://travis-ci.org/catdad/{{name}}
+
+[5]: https://codeclimate.com/github/{{user}}/{{name}}/badges/gpa.svg
+[6]: https://codeclimate.com/github/{{user}}/{{name}}
+
+[7]: https://img.shields.io/npm/dm/{{name}}.svg
+[8]: https://www.npmjs.com/package/{{name}}
+[9]: https://img.shields.io/npm/v/{{name}}.svg
+
+[10]: https://david-dm.org/{{user}}/{{name}}.svg
+[11]: https://david-dm.org/{{user}}/{{name}}
+`;
+
+_.templateSettings = {
+    evaluate : /\{\[([\s\S]+?)\]\}/g,
+    interpolate : /\{\{([\s\S]+?)\}\}/g
+};
+
+var template = _.template(badgesString);
+
+var pkg = {};
+
+try {
+    pkg = require('./package.json');
+} catch(e) {
+}
+
+var USER = process.env.USER || argv.user || 'catdad';
+var NAME = process.env.NAME || argv.name || pkg.name;
+
+console.log(template({
+    name: NAME,
+    user: USER
+}));
