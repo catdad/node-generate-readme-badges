1378647a66f94cbfdc892f4a2b8f782d9d3d9d0b
Kiril Vatev <vatev.1@gmail.com>
2017-03-14 20:20:16 -0400

    using badge link names instead of numbers
    #4

--- index.js
+++ index.js
@@ -35,26 +35,26 @@ if (
 
 // because why not
 console.log(`
-[![Build][1]][2]
-[![Test Coverage][3]][4]
-[![Code Climate][5]][6]
-[![Downloads][7]][8]
-[![Version][9]][8]
-[![Dependency Status][10]][11]
+[![Build][travis.svg]][travis.link]
+[![Test Coverage][cov-codeclimate.svg]][cov-codeclimate.link]
+[![Code Climate][gpa-codeclimate.svg]][gpa-codeclimate.link]
+[![Downloads][npm-downloads.svg]][npm.link]
+[![Version][npm-version.svg]][npm.link]
+[![Dependency Status][dm-david.svg]][dm-david.link]
 
-[1]: https://travis-ci.org/${USER}/${NAME}.svg?branch=master
-[2]: https://travis-ci.org/${USER}/${NAME}
+[travis.svg]: https://travis-ci.org/${USER}/${NAME}.svg?branch=master
+[travis.link]: https://travis-ci.org/${USER}/${NAME}
 
-[3]: https://codeclimate.com/github/${USER}/${NAME}/badges/coverage.svg
-[4]: https://codeclimate.com/github/${USER}/${NAME}/coverage
+[cov-codeclimate.svg]: https://codeclimate.com/github/${USER}/${NAME}/badges/coverage.svg
+[cov-codeclimate.link]: https://codeclimate.com/github/${USER}/${NAME}/coverage
 
-[5]: https://codeclimate.com/github/${USER}/${NAME}/badges/gpa.svg
-[6]: https://codeclimate.com/github/${USER}/${NAME}
+[gpa-codeclimate.svg]: https://codeclimate.com/github/${USER}/${NAME}/badges/gpa.svg
+[gpa-codeclimate.link]: https://codeclimate.com/github/${USER}/${NAME}
 
-[7]: https://img.shields.io/npm/dm/${NAME}.svg
-[8]: https://www.npmjs.com/package/${NAME}
-[9]: https://img.shields.io/npm/v/${NAME}.svg
+[npm-downloads.svg]: https://img.shields.io/npm/dm/${NAME}.svg
+[npm.link]: https://www.npmjs.com/package/${NAME}
+[npm-version.svg]: https://img.shields.io/npm/v/${NAME}.svg
 
-[10]: https://david-dm.org/${USER}/${NAME}.svg
-[11]: https://david-dm.org/${USER}/${NAME}
+[dm-david.svg]: https://david-dm.org/${USER}/${NAME}.svg
+[dm-david.link]: https://david-dm.org/${USER}/${NAME}
 `);
